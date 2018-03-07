e27477b4c7948e24fe7632b2105f0c19b0e62a2e
AVWORLD\rob9575 <rsantos@esri.com>
2018-03-07 09:09:31 -0600

    Dijit notes, exercise setup

---
+++ Chap3Dojo/js/app.js
@@ -0,0 +1,11 @@
+require([
+    'dojo/parser',
+    'dijit/layout/ContentPane',
+    'dijit/layout/BorderContainer',
+    'dijit/form/Button',
+    'dojo/domReady!',
+], function(
+    parser
+) {
+    parser.parse();
+});
