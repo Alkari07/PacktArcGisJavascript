75e52392e1ad219c776efbb3d9fdfa19790a6ba6
AVWORLD\rob9575 <rsantos@esri.com>
2018-03-08 10:39:41 -0600

    Custom dijit and events, file structuring

---
+++ Chap3Dojo/js/Census.js
@@ -0,0 +1,94 @@
+define([
+    'dojo/_base/declare',
+    'dijit/_WidgetBase',
+    'dijit/_TemplatedMixin',
+    'dijit/_OnDijitClickMixin',
+    'dojo/text!http://localhost:8080/js/templates/Census.html',
+    'dojo/dom-style',
+    'dojo/dom',
+    'esri/tasks/IdentifyTask',
+    'dojo/on', 
+    'dojo/_base/lang',
+    'esri/tasks/IdentifyParameters',
+    'dojo/_base/array',
+    'esri/InfoTemplate',
+    'dojo/text!http://localhost:8080/js/templates/StateCensus.html',
+    'dojo/text!http://localhost:8080/js/templates/CountyCensus.html',
+    'dojo/text!http://localhost:8080/js/templates/BlockGroupCensus.html',
+    'dojo/text!http://localhost:8080/js/templates/BlockCensus.html',],
+    function (declare, _WidgetBase, _TemplatedMixin, _OnDijitClickMixin, censusTemplate, domStyle,
+        dom, IdentifyTask, dojoOn, lang, IdentifyParameters,
+        arrayUtils, InfoTemplate, StateTemplate, CountyTemplate, BlockGroupTemplate, BlockTemplate) {
+        return declare ([_WidgetBase, _TemplatedMixin, _OnDijitClickMixin], {
+            templateString: censusTemplate,
+            baseClass: 'y2k-census',
+            show: function() {
+                
+                domStyle.set(this.domNode, 'display', 'block');
+                this._mapClickHandler = this.map.on('click', lang.hitch(this, this._onMapClick));
+            },
+            hide: function() {
+                domStyle.set(this.domNode, 'display', 'none');
+                if (this._mapClickHandler && this._mapClickHandler.remove) {
+                    this._mapClickHandler.remove();
+                }
+            },
+            constructor: function(options, srcRefNode) {
+                if (typeof srcRefNode ==="string") {
+                    srcRefNode = dom.byId(srcRefNode);
+                }
+                this.identifyTask = new IdentifyTask(options.mapService);
+                this.map = options.map || null;
+                this.domNode = srcRefNode;
+            },
+            _onMapClick: function(event) {
+                var params = new IdentifyParameters();
+                var defResults;
+
+                //the map is now a property of the class instance, and so needs to be referenced by this
+                params.geometry = event.mapPoint;
+                params.layerOption = IdentifyParameters.LAYER_OPTION_ALL;
+                params.mapExtent = this.map.extent;
+                params.returnGeometry = true;
+                params.width = this.map.width;
+                params.height = this.map.height;
+                params.spatialReference= this.map.spatialReference;
+                params.tolerance = 3;
+
+                this.map.graphics.clear();
+                defResults = this.identifyTask.execute(params).addCallback(
+                    lang.hitch(this, this._onIdentifyComplete)
+                );
+                this.map.infoWindow.setFeatures([defResults]);
+                this.map.infoWindow.show(event.mapPoint);
+            },
+            _onIdentifyComplete: function (results) {
+                return arrayUtils.map(results, function(result) {
+                    var feature = result.feature;
+                    var title = result.layerName;
+                    var content;
+                    
+                    switch(title) {
+                        case "Census Block Points":
+                            content = BlockTemplate;
+                            break;
+                        // case "Census Block Group":
+                        //     content = BlockGroupTemplate;
+                        //     break;
+                        // case "Detailed Counties":
+                        //     content = CountryTemplate;
+                        //     break;
+                        // case "states":
+                        //     content = StateTemplate;
+                        //     break;
+                        default:
+                            content = "${*}";
+                    }
+
+                    feature.infoTemplate = new InfoTemplate(title, content);
+                    return feature;
+                });
+            }
+        });
+    }
+);
--- Chap3Dojo/js/app.js
+++ Chap3Dojo/js/app.js
@@ -1,11 +1,31 @@
 require([
     'dojo/parser',
+    'esri/map',
+    'y2k/Census',
     'dijit/layout/ContentPane',
     'dijit/layout/BorderContainer',
     'dijit/form/Button',
+    'dojo/_base/lang',
+    'dijit/registry',
     'dojo/domReady!',
 ], function(
-    parser
+    parser, Map, Census, ContentPane, Bordercontainer, Button, lang, registry
 ) {
     parser.parse();
+
+    var map = new Map("map", {
+        basemap: "national-geographic",
+        center: [-95, 45],
+        zoom:3
+    });
+
+    var census = new Census({
+        map: map,
+        mapService:
+        'http://sampleserver6.arcgisonline.com/arcgis/rest/services/Census/MapServer/'
+    }, 'census-widget');
+
+    var censusBtn = registry.byId('census-btn');
+
+    censusBtn.on('click', lang.hitch(census, census.show));
 });
