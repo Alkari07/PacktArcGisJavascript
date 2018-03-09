e3086aba94e1c7b9a16ae0292eebafc5ca474bef
AVWORLD\rob9575 <rsantos@esri.com>
2018-03-09 13:44:24 -0600

    Added incident reporting attribution listeners

--- Chap5DataEditing/js/app.js
+++ Chap5DataEditing/js/app.js
@@ -11,10 +11,16 @@ require([
     'esri/layers/FeatureLayer',
     'esri/layers/ArcGISDynamicMapServiceLayer',
     'esri/symbols/SimpleMarkerSymbol',
+    'esri/config',
+    'esri/dijit/LocateButton',
+    'dojo/dom-construct',
+    'esri/dijit/AttributeInspector',
+    'dijit/form/Button',
     'dojo/domReady!'
 ], function(
     parser, Map, Graphic, Extent, Editor, TemplatePicker, Query, BorderContainer, ContentPane,
-    FeatureLayer, ArcGISDynamicMapServiceLayer, MarkerSymbol
+    FeatureLayer, ArcGISDynamicMapServiceLayer, MarkerSymbol, config, LocateButton,
+    domConstruct, AttributeInspector, Button
 ) {
     var maxExtent = new Extent({
         'xmin' : -13519092.335425414,
@@ -25,7 +31,7 @@ require([
             'wkid' : 102100
         }
     });
-    var map, selected, updateFeature, attInspector, incidentLayer, visibleIncidentLayer;
+    var map, selected, updateFeature, attInspector, incidentLayer, visibleIncidentLayer, selected;
 
     parser.parse();
     map = new Map("map", {
@@ -33,6 +39,8 @@ require([
         extent: maxExtent
     });
 
+    //et up a proxy for the feature layer
+    esriConfig.defaults.io.proxyUrl = './proxy/proxy.ashx';
     incidentLayer = new FeatureLayer('http://sampleserver3.arcgisonline.com/ArcGIS/rest/services/SanFrancisco/311Incidents/FeatureServer/0',
     {
         mode: FeatureLayer.MODE_SELECTION,
@@ -41,6 +49,136 @@ require([
     });
 
     visibleIncidentLayer = new ArcGISDynamicMapServiceLayer('http://sampleserver3.arcgisonline.com/ArcGIS/rest/services/SanFrancisco/311Incidents/MapServer');
+
+    
+    function generateAttributeInspector(layer) {
+        var layerInfo = [{
+            featureLayer: layer,
+            showAttachments: true,
+            isEditable: true,
+        }];
+
+        attInspector = new AttributeInspector({
+            layerInfos: layerInfo
+        }, domConstruct.create('div', null, document.body));
+        attInspector.startup();
+    
+        //add a save button next to the delete button
+        var saveButton = new Button({
+            label: "Save", 
+            class: "savebutton"
+        });
+        domConstruct.place(saveButton.domNode, attInspector.deleteBtn.domNode, 'after');
+    
+        saveButton.on('click', function() {
+            updateFeature.getLayer().applyEdits(null, [updateFeature], null);
+        });
+    
+        attInspector.on('attribute-change', function(evt) {
+            //store the updates to apply when the save button is clicked
+            updateFeature.attributes[evt.fieldname] = evt.fieldValue;
+        });
+    
+        attInsspector.on('next', function(evt) {
+            updateFeature = evt.feature;
+            console.log("Next: " + updateFeature.attributes.objectid);
+        });
+    
+        attInspector.on('delete', function(evt) {
+            evt.feature.getLayer().applyEdits(null, null, [updateFeature]);
+            map.infoWindow.hide();
+        });
+        if (attInspector.domNode) {
+            map.infoWindow.setContent(attInspector.domNode);
+            map.infoWindow.resize(350, 240);
+        }
+    }
+
+    
+
+    function showInspector(evt) {
+        var selectQuery = new Query();
+        var point = evt.mapPoint;
+        var mapScale = mapgetScale();
+
+        selectQuery.geometry = evt.mapPoint;
+
+        incidentLayer.selectFeatures(selectQuery, FeatureLayer.SELECTION_NEW, function (features) {
+            if (!features.length) {
+                map.infoWindow.hide();
+                return;
+            }
+
+            updateFeature = features[0];
+
+            map.infoWindow.setTitle(updateFeature.getLayer().name);
+            map.infoWindow.show(evt.screenPoint, map.getInfoWindowAnchor(evt.screenPoint));
+        
+        });
+    }
+
+    function generateTemplatePicker(layer) {
+        console.log('layer', layer);
+        var widget = new TemplatePicker({
+            featureLayers: [layer],
+            rows: layer.types.length,
+            columns: 1,
+            grouping: false,
+            style: "width: 98%;"
+        }, "editordiv");
+
+        widget.startup();
+
+        widget.on("selection-change", function() {
+            selected=widget.getSelected();
+            console.log("selected: ", selected);
+        });
+    }
+    function startEditing() {
+        //add locate button
+        var locator = new LocateButton({map: map}, "locatebutton");
+        var incidentLayer = map.getLayer('incidentLayer');
+        generateTemplatePicker(incidentLayer);
+        generateAttributeInspector(incidentLayer);
+        //add map click event to create th new editable feature
+        map.on('click', function(evt) {
+            // if a feature template has been selected.
+            if (selected) {
+                var currentDate = new Date();
+                var incidentAttributes = {
+                    req_type: selected.template.name,
+                    req_date: (currentDate.getMonth() + 1) + "/" + 
+                        currentDate.getDate() + "/" + currentDate.getFullYear(),
+                    req_time: currentDate.toLocaleTimeString,
+                    address: "",
+                    district: "",
+                    status: 1
+                };
+                var incidentGraphic = new Graphic(evt.mapPoint, selected.symbol, incidentAttributes);
+                incidentLayer.applyEdits([incidentGraphic,null,null]).then(function() {
+                showInspector(evt);  
+            });
+            } else {
+                showInspector(evt);
+            }
+        });
+
+        incidentLayer.setSelectionSymbol(
+            new MarkerSymbol({color: [255,0,0]})
+        );
+
+        map.infoWindow.on('hide', function() {
+            incidentLayer.clearSelection();
+        });
+
+        incidentLayer.on('click', showInspector);
+
+    }
+    incidentLayer.on('edits-complete', function() {
+        visibleIncidentLayer.refresh();
+    });
+
+    map.on('layers-add-result', startEditing);
 
     map.addLayers([visibleIncidentLayer, incidentLayer]);
 });
